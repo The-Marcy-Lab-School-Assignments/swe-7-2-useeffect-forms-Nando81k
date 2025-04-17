f5dd3084b78050eabc5218a592df7f361c8a6ed3
Nando <kommandernando@outlook.com>
2025-04-17 15:03:55 -0400

    Completed tasks and gifs render. Search form also is working

---
+++ app/config.js
@@ -0,0 +1,3 @@
+const API_KEY = 'sN4Fgerp0yC6mEp2Hw0VzqGDakOOusWM';
+
+export default API_KEY;
--- app/src/adapters/giphyAdapters.js
+++ app/src/adapters/giphyAdapters.js
@@ -5,16 +5,29 @@ TODO:
 - Import the API Key from your config.js file
 - Complete each adapter function to fetch from the trending/ and search/ endpoints. See the README for the complete endpoint URLs that you will be fetching from.
 */
-import { handleFetch } from './handleFetch.js'
+import { handleFetch } from './handleFetch.js';
+import API_KEY from '../../config.js'; // Import the API key from config.js
 
-const baseUrl = 'https://api.giphy.com/v1/gifs'
+const baseUrl = 'https://api.giphy.com/v1/gifs';
 
 // Send a fetch request to the /trending endpoint and return the top 3 results
 export const getTrendingGifs = async () => {
-
-}
+  const [data, error] = await handleFetch(
+    `https://api.giphy.com/v1/gifs/trending?api_key=${API_KEY}&limit=3`
+  );
+  if (error) {
+    return [[], error];
+  }
+  return [data.data.slice(0, 3), null];
+};
 
 // Send a fetch request to the /search endpoint with the given term as a query parameter
 export const getGifsBySearch = async (term) => {
-
-}
+  const [data, error] = await handleFetch(
+    `https://api.giphy.com/v1/gifs/search?api_key=${API_KEY}&q=${term}&limit=3`
+  );
+  if (error) {
+    return [[], error];
+  }
+  return [data.data.slice(0, 3), null];
+};
